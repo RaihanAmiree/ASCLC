c00d13ea4ad9feb99cc33e98e597f1587c7042e3
RaihanAmiree <raihan.a.amiree@gmail.com>
2025-11-14 16:51:13 +0600

    carousel

--- src/Components/InfoCarousel.jsx
+++ src/Components/InfoCarousel.jsx
@@ -12,25 +12,27 @@ import "slick-carousel/slick/slick-theme.css";
 
 function AutoPlay() {
   const settings = {
-    dots: false,
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    speed: 7000,
-    autoplaySpeed: 0,
-    cssEase: "linear",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: { slidesToShow: 2 },
-      },
-      {
-        breakpoint: 640,
-        settings: { slidesToShow: 1 },
-      },
-    ],
-  };
+  dots: false,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  speed: 7000,
+  autoplaySpeed: 0,
+  cssEase: "linear",
+  adaptiveHeight: true,
+  centerMode: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: { slidesToShow: 2 },
+    },
+    {
+      breakpoint: 640,
+      settings: { slidesToShow: 1 },
+    },
+  ],
+};
 
   // 👇 Array of InfoCard data
   const teamMembers = [
